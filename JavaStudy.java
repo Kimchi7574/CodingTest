625ac5da21fb9e7ebdcca8206e3e3d38ce440d5f
Kanghee <95953304+Kimchi7574@users.noreply.github.com>
2025-05-09 15:45:21 +0900

    programers_3

--- JavaStudy.java
+++ JavaStudy.java
@@ -19,18 +19,45 @@
 //        }
 //    }
 //}
-
+//
 //프로그래머스 피타고라스
+//import java.util.Scanner;
+//
+//public class Solution {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int c = sc.nextInt();
+//        a=a*a;c=c*c;
+//        int b_square = c - a;
+//
+//        System.out.println(b_square);
+//    }
+//}
+
+//프로그래머스
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int c = sc.nextInt();
-        a=a*a;c=c*c;
-        int b_square = c - a;
+        int year = sc.nextInt();
+        String age_type = sc.next();
+        int answer = 0;
 
-        System.out.println(b_square);
+        if (age_type.equals(
+                "Korea"
+        )) {
+            answer =
+                    2030-year+1
+            ;
+        }
+        else if (age_type.equals("Year")) {
+
+            answer = 2030 - year
+            ;
+        }
+
+        System.out.println(answer);
     }
 }
