0983d158d6f409220c70db0c2a836d25b1c937d1
Kanghee <95953304+Kimchi7574@users.noreply.github.com>
2025-05-09 15:35:34 +0900

    programers_2

--- JavaStudy.java
+++ JavaStudy.java
@@ -1,21 +1,36 @@
+//import java.util.Scanner;
+//
+//public class Solution {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String code = sc.next();
+//        String lastFourWords = code.substring(code.length() - 4);
+//
+//        if (lastFourWords.equals("_eye")) {
+//            System.out.println("Ophthalmology");
+//        } else if (lastFourWords.equals("head")) {
+//            System.out.println("Neurosurgery");
+//        } else if (lastFourWords.equals("infl")) {
+//            System.out.println("Orthopedics");
+//        } else if (lastFourWords.equals("skin")) {
+//            System.out.println("Dermatology");
+//        } else {
+//            System.out.println("direct recommendation");
+//        }
+//    }
+//}
+
+//프로그래머스 피타고라스
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String code = sc.next();
-        String lastFourWords = code.substring(code.length() - 4);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+        a=a*a;c=c*c;
+        int b_square = c - a;
 
-        if (lastFourWords.equals("_eye")) {
-            System.out.println("Ophthalmology");
-        } else if (lastFourWords.equals("head")) {
-            System.out.println("Neurosurgery");
-        } else if (lastFourWords.equals("infl")) {
-            System.out.println("Orthopedics");
-        } else if (lastFourWords.equals("skin")) {
-            System.out.println("Dermatology");
-        } else {
-            System.out.println("direct recommendation");
-        }
+        System.out.println(b_square);
     }
 }
